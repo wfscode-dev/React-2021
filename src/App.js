c0f71c7f6a03925fc78953d118702cc587ad70c8
wifisilence <wfscode@gmail.com>
2021-11-12 00:33:30 +0200

    comm
    Добавил MySelect, и сортировку

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect"
 
 function App() {
     //const [value, setValue] = useState('Text input')
@@ -18,17 +19,45 @@ function App() {
 
 
     //const bodyInputRef = useRef();//доступ напрямую к дом єлементу
+    const [selectedSort, setSelectedSort] = useState('')
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
     }
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    const sortPosts = (sort) => {
+        setSelectedSort(sort);
+        setPosts([...posts].sort((a,b)=> a[sort].localeCompare(b[sort])))
+    }
+
+
 
 
 
     return (
         <div className="App">
             <PostForm create={createPost}/>
-            <PostList posts={posts} title="Списочек"/>
+            <hr style={{margin: '15px 0'}}/>
+            <div>
+                <MySelect
+                value={selectedSort}
+                onChange={sortPosts}
+                defaultValue="Сортировка по..."
+                options={[
+                    {value: 'title', name: 'По названию'},
+                    {value: 'body', name: 'По описанию'}
+                ]}
+                />
+            </div>
+            {posts.length
+                ? <PostList remove={removePost} posts={posts} title="Списочек"/>
+                : <h1 style={{textAlign: "center"}}>Постов нет</h1>
+            }
+
         </div>
     )
 
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -11,7 +11,7 @@ const PostItem = (props) => {
                     </div>
                 </div>
                 <div className="post__btns">
-                    <MyButton>Удалить</MyButton>
+                    <MyButton onClick = {() => props.remove(props.post)}>Удалить</MyButton>
                 </div>
             </div>
     );
