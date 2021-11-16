cd20dee590ac59f60b432b1006d0c5f359e3d81c
wifisilence <wfscode@gmail.com>
2021-11-16 22:51:53 +0200

    comm
    Добавил модалку, повел декомпозицию

--- src/App.js
+++ src/App.js
@@ -8,76 +8,38 @@ import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect"
+import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/modal/MyModal";
+import {usePosts} from "./components/hooks/usePosts";
 
 function App() {
     //const [value, setValue] = useState('Text input')
-    const [posts, setPosts] = useState([
-        {id:1, title:'Javascript', body: 'Descriptions'},
-        {id:2, title:'Python', body: 'Descriptions1'},
-        {id:3, title:'Java', body: 'Descriptions2'},
-    ])
-
-
+    const [posts, setPosts] = useState([])
     //const bodyInputRef = useRef();//доступ напрямую к дом єлементу
-    const [selectedSort, setSelectedSort] = useState('')
-    const [searchQuery, setSearchQuery] = useState('')
+    const [filter, setFilter] = useState({sort:'',query: ''})
+    const [modal, setModal] = useState(false)
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
-
-    const sortedPosts = useMemo(() => {
-        console.log('Отработала')
-        if (selectedSort) {
-            return [...posts].sort((a,b)=> a[selectedSort].localeCompare(b[selectedSort]))
-        }
-        return posts;
-
-    }, [selectedSort, posts])
-
-    const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery.toLowerCase()))
-
-    }, [searchQuery, sortedPosts])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
+        setModal(false)
     }
 
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
     }
-
-    const sortPosts = (sort) => {
-        setSelectedSort(sort);
-    }
-
-
-
-
-
+//остановился на 1:36
     return (
         <div className="App">
-            <PostForm create={createPost}/>
-            <hr style={{margin: '15px 0'}}/>
-            <div>
-                <MyInput
-                    value = {searchQuery}
-                    onChange={e=>setSearchQuery(e.target.value)}
-                    placeholder = "Поиск..."
-                />
-                <MySelect
-                value={selectedSort}
-                onChange={sortPosts}
-                defaultValue="Сортировка по..."
-                options={[
-                    {value: 'title', name: 'По названию'},
-                    {value: 'body', name: 'По описанию'}
-                ]}
-                />
-            </div>
-            {sortedAndSearchedPosts.length
-                ? <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Списочек"/>
-                : <h1 style={{textAlign: "center"}}>Постов нет</h1>
-            }
+            <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>Создать пост</MyButton>
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm create={createPost}/>
+            </MyModal>
 
+            <hr style={{margin: '15px 0'}}/>
+            <PostFilter filter={filter} setFilter={setFilter}/>
+            <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Списочек"/>
         </div>
     )
 
--- src/components/PostFilter.jsx
+++ src/components/PostFilter.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import MySelect from "./UI/select/MySelect";
 import MyInput from "./UI/input/MyInput";
 
-const PostFilter = () => {
+const PostFilter = ({filter, setFilter}) => {
     return (
         <div>
             <MyInput
-                value={searchQuery}
-                onChange={e=> setSearchQuery(e.target.value)}
+                value={filter.query}
+                onChange={e=> setFilter({...filter, query: e.target.value})}
                 placeholder="Поиск..."
             />
             <MySelect
-                value={selectedSort}
-                onChange={sortPosts}
+                value={filter.sort}
+                onChange={selectedSort => setFilter({...filter, sort: selectedSort})}
                 defaultValue="Сортировка по..."
                 options={[
                     {value: 'title', name: 'По названию'},
