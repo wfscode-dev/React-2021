515c4422bb258ab53face0004c204d75cf4add3f
wifisilence <wfscode@gmail.com>
2021-11-18 22:55:23 +0200

    comm
    Добавили APi и пагинацию

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useMemo, useState, useRef} from "react";
+import React, {useMemo, useState, useRef, useEffect} from "react";
 //import Counter from "./components/Counter";
 //import ClassCounter from "./components/ClassCounter"
 import './styles/App.css'
@@ -10,15 +10,35 @@ import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect"
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/modal/MyModal";
-import {usePosts} from "./components/hooks/usePosts";
+import {usePosts} from "./hooks/usePosts";
+import axios from "axios";
+import PostServise from "./API/PostServise";
+import Loader from "./components/UI/Loader/Loader";
+import {useFetching} from "./hooks/useFetching";
+import {getPageCount, getPagesArray} from "./utils/pages";
 
 function App() {
     //const [value, setValue] = useState('Text input')
     const [posts, setPosts] = useState([])
     //const bodyInputRef = useRef();//доступ напрямую к дом єлементу
-    const [filter, setFilter] = useState({sort:'',query: ''})
+    const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
+    const [totalPages, setTotalPages] = useState(0)
+    const [limit, setLimit] = useState(10)
+    const [page, setPage] = useState(1)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+    let pagesArray = getPagesArray(totalPages);
+    console.log(pagesArray)
+    const [fetchPosts, isPostsLoading, postError  ] = useFetching(async (limit, page) => {
+        const response = await PostServise.getAll(limit, page);
+        setPosts(response.data)
+        const totalCount = response.headers['x-total-count']
+        setTotalPages(getPageCount(totalCount, limit))
+    })
+
+    useEffect(() => {
+        fetchPosts(limit, page)
+    }, [])
 
 
     const createPost = (newPost) => {
@@ -26,25 +46,44 @@ function App() {
         setModal(false)
     }
 
+
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
     }
-//остановился на 1:36
+
+    const changePage = (page) => {
+        setPage(page)
+        fetchPosts(limit, page)
+    }
+
     return (
         <div className="App">
             <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>Создать пост</MyButton>
             <MyModal visible={modal} setVisible={setModal}>
                 <PostForm create={createPost}/>
             </MyModal>
-
             <hr style={{margin: '15px 0'}}/>
             <PostFilter filter={filter} setFilter={setFilter}/>
-            <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Списочек"/>
+            {postError &&
+            <h1>ERROR</h1>}
+            {isPostsLoading
+                ?<div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
+                :<PostList remove={removePost} posts={sortedAndSearchedPosts} title="Списочек"/>
+            }
+            <div className="page__wrapper">
+                {pagesArray.map(p =>
+                    <span
+                        onClick={() => changePage(p)}
+                        key={p}
+                        className={page === p? "page page__current" :'page'}>{p}</span>
+                )}
+            </div>
+
         </div>
     )
 
+//закончил 2:10
 
-
-}
+};
 
 export default App;
